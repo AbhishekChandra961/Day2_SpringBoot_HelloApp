0ae909f4b72d8149a9f75a44b1d27259bdf9b04c
Abhishek <achandra961@gmail.com>
2023-04-09 23:06:14 +0530

    UC3

--- src/main/java/com/bridgelabz/HelloApp/controller/HelloController.java
+++ src/main/java/com/bridgelabz/HelloApp/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.HelloApp.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,10 @@ public class HelloController {
     }
     @RequestMapping(value="/hello/query",method = RequestMethod.GET)
     public String message1(@RequestParam String name){
+        return name;
+    }
+    @RequestMapping(value="/hello/param/{name}",method = RequestMethod.GET)
+    public String message2(@PathVariable String name){
         return name;
     }
 }
